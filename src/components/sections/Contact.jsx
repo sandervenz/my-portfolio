ba4960234543e3b7122c61ddc9247823a198b9fa
Sander <sandervdb23@gmail.com>
2025-04-15 22:09:55 +0700

    Refactor MobileMenu component by removing unused import and simplify promise handling in Contact component

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -19,7 +19,7 @@ export const Contact = () => {
         e.target,
         import.meta.env.VITE_PUBLIC_KEY
       )
-      .then((result) => {
+      .then(() => {
         alert("Message Sent!");
         setFormData({ name: "", email: "", message: "" });
       })
